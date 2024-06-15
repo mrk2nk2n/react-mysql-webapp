9c0250a3d8490355283766ebb6cba5e7b6d3271c
mrk2nk2n <jinhong.ken@gmail.com>
2024-06-15 11:55:52 +0800

    updated to use without .env file and use a new mysql password

--- backend/connect.js
+++ backend/connect.js
@@ -1,11 +1,11 @@
 import mysql from "mysql"
 
 let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"Password!",
+    database:"test",
   });
 
   connection.connect((err) => {
